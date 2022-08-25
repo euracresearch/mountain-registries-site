bfe85d02e360f377955b9d0fbdc23fe3ba2b67b8
Martin Palma <martin.palma@eurac.edu>
2022-08-25 14:00:02 +0200

    add github action for deploying to gh-pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://euracresearch.github.io',
+  base: '/mountain-registries-site',
   integrations: [tailwind({
     config: { applyBaseStyles: false },
   })]
