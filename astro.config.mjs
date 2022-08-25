e2e23967ed2345f5125070a291ed6491c0240815
Martin Palma <martin.palma@eurac.edu>
2022-08-25 14:58:36 +0200

    change site config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://euracresearch.github.io',
-  base: '/mountain-registries-site',
+  site: 'https://www.mountain-registries.org',
   integrations: [tailwind({
     config: { applyBaseStyles: false },
   })]
